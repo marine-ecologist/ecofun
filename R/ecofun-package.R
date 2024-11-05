be99cd4f43099bf659dc46e62af5926ed491e06f
marine-ecologist <george.roff@csiro.au>
2024-11-05 14:44:37 +1000

    Increment version number to 0.0.0.9005

--- R/ecofun-package.R
+++ R/ecofun-package.R
@@ -3,6 +3,8 @@
 
 ## usethis namespace: start
 #' @importFrom INLA inla.stack.index
+#' @importFrom stats sd
+#' @importFrom stats terms
 #' @importFrom tictoc tic
 #' @importFrom tictoc toc
 ## usethis namespace: end
